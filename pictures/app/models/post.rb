d5846ef91cb6054b0a3af3c402d2910a53fb2f80
haruki <Haruki.Miyagi1112@gmail.com>
2018-04-14 23:36:48 +0900

    Styling and ユーザーのリンク

--- pictures/app/models/post.rb
+++ pictures/app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :title, presence: true
   validates :image, presence: true
+  validates :user_id, presence: true
+  belongs_to :user
 end
